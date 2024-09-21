2109bcce3313255b7d50777733fefa5515d166aa
alanakov <angelikovalski@gmail.com>
2024-09-21 16:56:46 -0300

    configurando o php

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
-$conteudo = file_get_contents("http://localhost:4567"); 
-$arrayAssociativo = json_decode($conteudo);
+session_start();
+
+if(isset($_GET['comando']) && ($_GET['save'])) {
+
+    $command = rawurlencode($_GET['comando']);
+    $save - rawurlencode($_GET['save']);
+    $content = file_get_contents("http://localhost:4567/{$command}/{$save}");
+
+} else if (isset($_GET['comando'])) {
+
+    $command = rawurlencode($_GET['comando']);
+    $content = file_get_contents("http://localhost:4567/{$command}");
+
+} else {
+
+    $content = file_get_contents("http://localhost:4567"); 
+}
+
+
+$arrayAssociative = json_decode($content);
+$_SESSION['history'] = isset($_SESSION['history']) ? array_merge($_SESSION['history'], $arrayAssociative->messages) : [];
+$messages = $_SESSION['history'];
 
 include "template.phtml";
